5bbad5ebb1870f5f382c2d3c2a615f5c5215743a
Olga Koval <ok@telemark-it.ru>
2017-08-23 10:40:29 +0300

    catalog styles

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 ymaps.ready(function () {
-  var myMap = new ymaps.Map('map', {
+  var map = new ymaps.Map('map', {
     center: [59.939390, 30.329545],
     zoom: 16,
     scrollZoom: false,//зум при скролле мышью отключен
@@ -7,7 +7,7 @@ ymaps.ready(function () {
   }, {
     searchControlProvider: 'yandex#search'
   }),
-  myPlacemark1 = new ymaps.Placemark([59.938631, 30.323055], {
+  Placemark = new ymaps.Placemark([59.938631, 30.323055], {
     balloonContent: 'Санкт-Петербург, Большая Конюшенная улица, 19/8'
   }, {
     // Опции.
@@ -20,9 +20,10 @@ ymaps.ready(function () {
     // её "ножки" (точки привязки).
     iconImageOffset: [-40, -140]
   });
-  myMap.geoObjects.add(myPlacemark1);
-  myMap.behaviors.disable('scrollZoom');
-  myMap.controls.add('zoomControl');
-  var roadcontrolState = myMap.controls.get('zoomControl').state.get('size');
-  myMap.controls.get('zoomControl').options.set('size', 'small');
+
+  map.geoObjects.add(Placemark);
+  map.behaviors.disable('scrollZoom');
+  map.controls.add('zoomControl');
+  var roadcontrolState = map.controls.get('zoomControl').state.get('size');
+  map.controls.get('zoomControl').options.set('size', 'small');
 });
