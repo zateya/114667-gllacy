0a3b2948f0e771d95691eeaa7ee6904d5e876c65
Olga Koval <ok@telemark-it.ru>
2017-08-30 23:10:01 +0300

    js, map, buttons

--- js/script.js
+++ js/script.js
@@ -13,31 +13,75 @@
   var storageEmail = localStorage.getItem("clientEmail");
 
   if (feedbackLink) {
-  feedbackLink.addEventListener("click", function(event) {
-   event.preventDefault();
-   if (pageOverlay) {
-     pageOverlay.classList.remove("page-overlay--hide");
-     pageOverlay.classList.add("page-overlay--show");
-   };
-   feedbackPopup.classList.add("popup--opened");
-   clientName.focus();
+    feedbackLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    if (pageOverlay && pageOverlay.classList.contains("page-overlay--hide")) {
+       pageOverlay.classList.remove("page-overlay--hide");
+       pageOverlay.classList.add("page-overlay--show");
+    };
+    feedbackPopup.classList.remove("popup--closed");
+    feedbackPopup.classList.add("popup--opened");
+    clientName.focus();
+
+    if (storageName && !storageEmail) {
+         clientName.value = storageName;
+         clientEmail.focus();
+     } else if (storageName && storageEmail) {
+         clientName.value = storageName;
+         clientEmail.value = storageEmail;
+         clientMessage.focus();
+     } else {
+         clientName.focus();
+     };
+  });
+
+  feedbackForm.addEventListener("submit", function(event) {
+    if (clientName.value && clientEmail.value && clientMessage.value) {
+      localStorage.setItem('clientName', clientName.value);
+      localStorage.setItem('clientEmail', clientEmail.value);
+    }
   });
 
   feedbackClose.addEventListener("click", function(event) {
     event.preventDefault();
     feedbackPopup.classList.remove("popup--opened");
-    pageOverlay.classList.add("page-overlay--hide");
-    pageOverlay.classList.remove("page-overlay--show");
+    feedbackPopup.classList.add("popup--closed");
+    feedbackPopup.classList.remove("popup--error");
+
+    if (pageOverlay && pageOverlay.classList.contains("page-overlay--show")) {
+      pageOverlay.classList.remove("page-overlay--show");
+      pageOverlay.classList.add("page-overlay--hide");
+    };
   });
 
   if (pageOverlay) {
     pageOverlay.addEventListener("click", function(event) {
-       event.preventDefault();
-       feedbackPopup.classList.remove("popup--opened");
-       pageOverlay.classList.add("page-overlay--hide");
-       pageOverlay.classList.remove("page-overlay--show");
-     });
+      event.preventDefault();
+      if (feedbackPopup.classList.contains("popup--opened")) {
+        feedbackPopup.classList.remove("popup--opened");
+        feedbackPopup.classList.add("popup--closed");
+        feedbackPopup.classList.remove("popup--error");
+      };
+      if (pageOverlay.classList.contains("page-overlay--show")) {
+        pageOverlay.classList.remove("page-overlay--show");
+        pageOverlay.classList.add("page-overlay--hide");
+      };
+    });
   };
+
+  window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+      if (feedbackPopup.classList.contains("popup--opened")) {
+        feedbackPopup.classList.remove("popup--opened");
+        feedbackPopup.classList.add("popup--closed");
+        feedbackPopup.classList.remove("popup--error");
+      };
+      if (pageOverlay.classList.contains("page-overlay--show")) {
+        pageOverlay.classList.remove("page-overlay--show");
+        pageOverlay.classList.add("page-overlay--hide");
+      };
+    };
+  });
  };
 
   ymaps.ready(function () {
@@ -60,7 +104,13 @@
       iconImageSize: [80, 140],
       // Смещение левого верхнего угла иконки относительно
       // её "ножки" (точки привязки).
-      iconImageOffset: [-40, -140]
+      iconImageOffset: [-40, -140],
+      // Добавляем иконку тени и задаем аналогичные параметры.
+      iconShadow: true,
+      iconShadowLayout: 'default#image',
+      iconShadowImageHref: 'img/icons/pin-shadow.png',
+      iconShadowImageSize: [182, 110],
+      iconShadowImageOffset: [0, -110]
     });
 
     map.geoObjects.add(Placemark);
